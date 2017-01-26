edf460407c8054ada97b396c3ba2b1d901253190
Alexander Ogle <Alexander Ogle>
2017-01-26 10:28:58 -0800

    Wrote function which behaves as a wait timer, wrote function for 2 separate light sequences on the MSP-EXPG2 board with MSP2553 installed.

--- main.c
+++ main.c
@@ -6,24 +6,69 @@
 //------------------------------------------------------------------------------
 #include <io430.h>                     // Specific device header
 
+//function declaration for the light show
+void lightShow(int);
+
 int main(void) {
   volatile int i;
   // stop watchdog timer
   WDTCTL = WDTPW | WDTHOLD;
-  // set up P1.0 (LED1) as output
-  P1DIR = 0x01;
-  // intialize P1.0 (LED1) to 0 (off)
+  // set up P1.0 (LED1) and P1.6 as output
+  P1DIR = 0x41;
+  // intialize all pins to 0 (off)
   P1OUT = 0x00;
   
   // loop forever 
   for(;;) {
-    //toggle LED1 on P1.0
-    P1OUT ^= 0x01;
+    lightShow(2);
+    
+    /* Legacy Code 2017-01-26 1016 PST
+    //toggle LED1 on P1.0 and LED2 on P1.6
+    P1OUT ^= 0x41;
     // Wait! We need to see it at least, yo
     for (i = 0; i < 0x6000; i++){
       // We're just chilling in here until i increments past 0x6000
     }
+    */
   }
   
 }
 
+void lightShow(int sequence){
+  // Function for implementing different light shows using a single function
+  
+  //We declare the functions used in this program first;
+  void waitForIt(int);
+  
+  if(sequence == 1){
+    P1OUT = 0x41;
+    waitForIt(500);
+    P1OUT = 0x00;
+    waitForIt(500);
+  }
+  else if(sequence == 2){
+    P1OUT = 0x01;
+    waitForIt(500);
+    P1OUT = 0x41;
+    waitForIt(500);
+    P1OUT = 0x40;
+    waitForIt(500);
+    P1OUT = 0x00;
+    waitForIt(500);
+  }
+}
+
+void waitForIt(int time){
+  // A value of 0x6000 in the for loop results in a wait speed of about 
+  // a half second. We'll use that to calculate the wait time in terms of
+  // milliseconds. (0x6000)base16 == (24,576)base10
+  int thisTime = 0;
+  //calculate the number of milliseconds required for the timer
+  for(int i = 0; i < time; i++){
+    // A millisecond is about 49.152 iterations within the for loop
+    thisTime += 49;
+  }
+  for(int i = 0; i < thisTime; i++){
+    //Do nothing, just let the CPU run while we wait
+  }
+}
