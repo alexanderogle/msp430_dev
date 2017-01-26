1fd3b0273988aea30c1998e01981d2a5e6ad619a
Alexander Ogle <Alexander Ogle>
2017-01-25 16:14:17 -0800

    First working blinky sketch using the IAR embedded workbench IDE version 6.50.1

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
+//------------------------------------------------------------------------------
+// ledson.c - simple program to light LEDs
+// Set pins to output, lights pattern of LEDs, then loops forever
+// MSP-EXP430G board with LEDs active low(or is it high?) on P1.0 and P1.6
+// A J Ogle, 2017-01-25; IAR Kickstart version 6.50.4
+//------------------------------------------------------------------------------
+#include <io430.h>                     // Specific device header
 
-#include "io430.h"
-
-int main( void )
-{
-  // Stop watchdog timer to prevent time out reset
-  WDTCTL = WDTPW + WDTHOLD;
-
-  return 0;
+int main(void) {
+  volatile int i;
+  // stop watchdog timer
+  WDTCTL = WDTPW | WDTHOLD;
+  // set up P1.0 (LED1) as output
+  P1DIR = 0x01;
+  // intialize P1.0 (LED1) to 0 (off)
+  P1OUT = 0x00;
+  
+  // loop forever 
+  for(;;) {
+    //toggle LED1 on P1.0
+    P1OUT ^= 0x01;
+    // Wait! We need to see it at least, yo
+    for (i = 0; i < 0x6000; i++){
+      // We're just chilling in here until i increments past 0x6000
+    }
+  }
+  
 }
+
